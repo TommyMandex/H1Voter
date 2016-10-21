4cbe9926d165b7ab3b155612809fe38dc392df62
YasserGersy <gersy.ch2@gmail.com>
2016-10-21 19:35:01 +0200

    New 400 ids

--- register.py
+++ register.py
@@ -3,7 +3,20 @@ import datetime,time,os
 import time
 import random, string
 
+debugmode=False
 requests.packages.urllib3.disable_warnings()
+
+
+
+def randomNumber(l):
+	s=string.digits
+	return ''.join(random.sample(s,l))
+
+def randomstr(l):
+	s=string.digits+string.lowercase+string.uppercase
+	return ''.join(random.sample(s,l))
+
+
 os.system('rm -rf errors')
 class stx:    
 	RED='\033[1;31m'
@@ -17,7 +30,7 @@ class stx:
 	crims=' \033[1;38m'
 	magenta='\033[1;35m'									
 	lin='\n---------------------------------------------------------------------------------\n'
-
+randomNames=['hany_ramzy','adel_emam','ghada_abdel_razek','youssra','Abla_kamel','Mona_zaki','abu_trika','hossam_hassan','mofeed_fawzy','samira_sa3eed','Hend_rostom','sara_jay','anjelica','sophie_dee','maysa','mamdooh','bill_gates','Abd_elfattah','taha_hussien','john','levis','brad','max','haven','tapol','lexi','sam','michil','henry','chris','frank','jobert','steven','dep','smith','franklein','zak','josef','merlin','donald','barak','adel','sarah','taison','fawzya','nermeen','hend','zakaria','yasser','emad']
 #Testing only
 proxyDict = { "http"  : "http://127.0.0.1:8080", "https" : "https://127.0.0.1:8080",   "ftp"   : "ftp://127.0.0.1:8080"}
 cokstr="__cfduid=d1cdfc0b504bd870dc94c5509a2f41bd71472917466; _ga=GA1.2.1635264352.1472917475; __Host-session=anhEcDkzOVVwYWxDL2pVSTJ3NTZWeHFVUzhTTzVHbDNzK1lQUWl3eG53Q0xyNHdLY3Y5aHJsQnl4VUNtSjI4OWx6bFozRDZDcFBRQVlXdjBsMXpaUEFNR3ZpTFV6c1NVU2s0TnBvMFhVZTAxWEVyUXVQT2FDSExUNjFZQW16UG1IWWlxaE9Zd0h1SFNmKytFWW52eG81N0xqWTh1QW16bS9leThwME1sQXllWE00bHJCN2NqNTlMN3pWckN2eXFULS1qcFJoTW1SSVF6VkVVQXllT1QwUy93PT0%3D--94bd223fffd82f1c61b2d03cc2f0be5ac1f3b93a; _gat=1"
@@ -37,17 +50,75 @@ if len(f)==0:
 	print('Starting from 0')
 else :
 	f=int(f)
-t=raw_input('Emails count (ex: 100) : ')
-if len (t) ==0:
-	t=100
-	print('count=100')
-else :
-	t=int(t)
-name=raw_input(stx.yel+'\nWhat is Your name?: ')
-if len(name) ==0 :
-	msx=''.join(random.choice(string.lowercase) for i in range(5))
-	name='BotX'+msx+'N'
-	print ('Your new name is '+name)
+t=0
+donet=False
+while donet==False:
+	donet=True
+	t=raw_input('Emails count (ex: 100 or max) : ')
+	if len (t) ==0:
+		t=100
+		t='max'
+		print('count=100')
+	elif t.isdigit():
+		t=int(t)
+	elif t!='max':
+		donet=False
+
+validchoice=False
+xc=''
+ea=''
+emailsList=[]
+sufx=''
+FromFile=False
+while  validchoice==False:
+	xc=raw_input('\n Choose (1/2)\n1- Random emails\n2-From File\n:')
+	if xc.isdigit():
+		xc=int(xc)
+		validchoice=True
+	else:
+		validchoice=False
+
+if xc==1:
+	FromFile=False
+	if t=='max':
+		t=100
+	name=raw_input(stx.yel+'\nWhat is Your name?: ')
+	if len(name) ==0 :
+		msx=''.join(random.choice(string.lowercase) for i in range(5))
+		name='BotX'+msx+'N'
+		print ('Your new name is '+name)
+	ea=raw_input(stx.brown+'\nWhere are your emails registered "email domain"?\n (ex: yopmail.com): ')
+	if ea.startswith('@'):
+		ea=ea[1:]
+
+	if len(ea) ==0:
+		ea='yopmail.com'
+		print('Email domain set to "'+ea+'"')
+	elif '.' not in ea:
+		print 'Invalid email domain name\n 		Valid example: "yopmail.com"'
+		exit()
+
+	sufx=raw_input('\nIf you want to add sufx type it :')
+else:
+	FromFile=True
+	pathofemails=raw_input('Where is the emails file located ?')
+	if os.path.isfile(pathofemails)==False:
+		print('non existed file ['+pathofemails+']\nleaving...')
+		exit(0)
+	else:
+		try:
+			with open(pathofemails) as reader:
+				emailsList=reader.readlines()
+			if len(emailsList)<1:
+				print('\n empty file \n leaving ...')
+				exit()
+
+		except Exception :
+			ger=1
+	if t=='max' :
+		t=len(emailsList)
+	elif t> len(emailsList):
+		t=len(emailsList)
 
 pas=raw_input(stx.Green+'\nWhat is your New password: ')
 if len(pas)==0:
@@ -56,18 +127,8 @@ if len(pas)==0:
 elif len(pas) < 8:
 	print(stx.RED+'Password minmum 8 chars ')
 	exit()
-ea=raw_input(stx.brown+'\nWhere are your emails registered "email domain"?\n (ex: yopmail.com): ')
-if ea.startswith('@'):
-	ea=ea[1:]
 
-if len(ea) ==0:
-	ea='yopmail.com'
-	print('Email domain set to "'+ea+'"')
-elif '.' not in ea:
-	print 'Invalid email domain name\n 		Valid example: "yopmail.com"'
-	exit()
 
-sufx=raw_input('\nIf you want to add sufx type it :')
 ty=raw_input(stx.magenta+'\n If you want to add new  cookis \n paste it  , if not press Enter :')
 if len(ty) >0:
 	cokstr=ty
@@ -87,29 +148,68 @@ h = {
 ,"Cookie":cokstr 
 }
 
-print'\n -------------------------------------------------------------------------\n'
-print(stx.Blue+'\n 				Starting registeration\n'+stx.yel)
 ids=''
 end=f+t
 
+print'\n -------------------------------------------------------------------------\n'
+print(stx.Blue+'\n 				Starting registeration '+str(f)+':'+str(end)+'\n'+stx.yel)
+
+FroCount=-1
+if FromFile and end >= len(emailsList):
+	end=len(emailsList)-1
 for u in range(f,end):
+	FroCount=FroCount+1
 	reg=False
+
 	while reg ==False:
-		s=name+str(u)+sufx
-		print(stx.yel+'Registering with ['+s+'@'+ea+']')
-		da="user[name]=Egy+Bots&user[username]="+s+"&1=iiiiiiiiiiiiiiiiiiiiiiii&user[email]="+s+"%40"+ea+"&user[password]="+pas+"&user[password_confirmation]="+pas
-		r =requests.post(url=url,data=da,headers=h)#,proxies=proxyDict)
+		if FromFile:
+			compemail=emailsList[u].strip()
+			if '@' not in compemail:
+				print('invalid email '+compemail)
+				break
+			else:
+				#s=compemail.split('@')[0].strip()
+				randint=randomNumber(2)
+				randint=int(randint)+5
+				while randint >= len(randomNames):
+					randint=randint-1
+				s=randomNames[randint]+'_'+randomNumber(5)
+		else:
+			s=name+str(u)+sufx
+			compemail=s+'@'+ea
+
+		encodedEmail=((s+'%40'+ea) if FromFile==False else (compemail.strip().replace('@','%40')))
+		print(stx.yel+str(FroCount+1)+' Registering with ['+compemail+']'+stx.Green)
+		s=s.replace('.','')
+		if len(s) > 25:
+			s=s[0:25]
+
+		da="user[name]=Egy+Bots&user[username]="+s+"&1=iiiiiiiiiiiiiiiiiiiiiiii&user[email]="+encodedEmail+"&user[password]="+pas+"&user[password_confirmation]="+pas
+		noerror=False
+		r=''
+		while noerror==False:
+			noerror=True
+			try:	
+				if debugmode:
+					r =requests.post(url=url,data=da,headers=h,proxies=proxyDict)
+				else:
+					r =requests.post(url=url,data=da,headers=h)#,proxies=proxyDict)
+			except Exception:
+				noerror=False
+				print('Error ... Retrying again ')
+				time.sleep(3)
+
 		usernameemailstatue=''
 		if 'been taken' in r.text:
 			if 'username'in r.text:
-				usernameemailstatue='    Username Reserved '
+				usernameemailstatue='    Username Reserved ['+s+']  '
 			if 'email' in r.text:
 				usernameemailstatue = usernameemailstatue+'\n    Email : registered before\n'
 			print(stx.RED+usernameemailstatue)
 			reg=True
 		
 		elif  'redirect_path":"/users/sign_in","errors":{}}' in r.text :
-			print stx.Green+'Register  succeeded  ['+s+"]\n"
+			print stx.Green+'Register  succeeded  ['+compemail+"]\n"
 			#x.append(s)
 			reg =True
 			ids=s+'@'+ea+':'+pas+'\n'
@@ -127,7 +227,7 @@ for u in range(f,end):
 		else:
 			print r.text
 	if f!=end:	
-		time.sleep(3)
+		time.sleep(1)
 rand=datetime.datetime.now()
 path='Bots_'+str(rand)
 path=path.replace(' ','').replace(':','').replace('-','').replace('.','')+"_.txt"
